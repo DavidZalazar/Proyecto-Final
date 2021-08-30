9e111e345de8cbc4c14127fa777dff5eefd5aff0
DavidZalazar <80550591+DavidZalazar@users.noreply.github.com>
2021-08-29 23:11:44 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.scss';
+import Header from "./containers/header/Header.js";
+import Hero from './components/hero/Hero.js';
+import About from './components/about/About.js';
+import Products from './components/products/Products.js';
+import Services from './components/services/Services.js';
+import Contact from './containers/contact/Contact.js';
+import Footer from './components/footer/Footer.js';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <About />
+      <Products />
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
